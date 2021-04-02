0b5fc842d6c3a0be9c58c66aa5c749d9579c51b9
fboyle2001 <finlayboyle2001@gmail.com>
2021-04-02 20:33:25 +0100

    Rewrote everything so far to tidy it up

--- sentiment_polarity.py
+++ sentiment_polarity.py
@@ -1,182 +1,178 @@
-import gensim.downloader as api
-import gensim.models
 from gensim.models.word2vec import Word2Vec, KeyedVectors
-import time
 from sklearn.decomposition import PCA
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-positive_lexicon_path = "./opinion-lexicon-English/positive-words.txt"
-negative_lexicon_path = "./opinion-lexicon-English/negative-words.txt"
+# Dictionary of files that may be used throughout the program
+FILE_PATHS = {
+    "pretrained-w2v-google-news": "pretrained-word2vec-keyedvectors.kv",
+    "positive_words": "./opinion-lexicon-English/positive-words.txt",
+    "negative_words": "./opinion-lexicon-English/negative-words.txt"
+}
 
-def create_lexicon_list(wvs, filepaths):
-    cleaned_words = []
+"""
+Loads a file of words from the Sentiment Lexicon (CITATION NEEDED)
 
-    for filepath in filepaths:
-        skipped = []
+word_vectors - The word2vec pre-trained model
+filepath - The location of the lexicon file
+"""
+def load_lexicon(word_vectors, filepath):
+    lexicon = set()
 
-        with open(filepath, "r") as file:
-            for line in file.readlines():
-                # The comments in the file start with a ;
-                if line.startswith(";"):
-                    continue
+    with open(filepath, "r") as file:
+        for line in file.readlines():
+            # Lines beginning with ; are comments
+            if line.startswith(";"):
+                continue
 
-                # They end with \n so strip any whitespace
-                cleaned_word = line.strip()
+            # Strip any whitespace i.e. \n
+            cleaned = line.strip()
 
-                if len(cleaned_word) == 0:
-                    continue
+            # If the line was entirely whitespace or is blank then discard it
+            if len(cleaned) == 0:
+                continue
 
-                for op in [1, 2, 3]:
-                    temp = ""
+            # I want to consider different variation of the words casing
+            case_candidates = [cleaned.lower(), cleaned.title(), cleaned.upper()]
 
-                    if op == 1:
-                        temp = cleaned_word.lower()
-                    elif op == 2:
-                        temp = cleaned_word.title()
-                    elif op == 3:
-                        temp = cleaned_word.upper()
+            for case_candidate in case_candidates:
+                # I also need to consider the different ways that hyphens can be handled
+                # I try 3 different possibilities here
+                candidates = [
+                    case_candidate.replace("-", "_"),
+                    case_candidate.replace("-", " "),
+                    case_candidate.replace("-", "")
+                ]
 
-                    if temp not in wvs:
-                        temp = temp.replace("-", "_")
+                # I then check if any of the candidates are in the trained word2vec model
+                # If so I can use this in the lexicon
+                for candidate in candidates:
+                    if candidate not in word_vectors:
+                        continue
 
-                        if temp not in wvs:
-                            temp = temp.replace("_", " ")
+                    # Using a set will prevent duplicates
+                    lexicon.add(candidate)
 
-                            if cleaned_word not in wvs:
-                                temp = temp.replace(" ", "")
+    return lexicon
 
-                                if temp not in wvs:
-                                    skipped.append(temp)
-                                    continue
+"""
+Produce a matrix consisting of the vectors retrieved from the word2vec model
+for the words provided.
 
-                    cleaned_words.append(temp)
+Each row is a sample and each column is a feature.
 
-        t = filepath.split('/')[-1]
-        with open(f"skipped_{t}.txt", "w") as f:
-            f.write("\n".join(skipped))
-
-    return cleaned_words
-
-def construct_w2v_matrix(wvs, words):
+word_vectors - The word2vec pre-trained model
+lexicon - A list of words
+"""
+def construct_w2v_matrix(word_vectors, lexicon):
     rows = []
 
-    for word in words:
-        if word not in wvs:
+    for word in lexicon:
+        # If it not in the model then we won't be able to get the vector
+        if word not in word_vectors:
             continue
 
-        rows.append(wvs.get_vector(word))
+        # Put the vector in as a row
+        rows.append(word_vectors.get_vector(word))
 
+    # Convert to a numpy array so we can use it with PCA
     return np.array(rows)
 
-# Load the vectors from word2vec trained on Google News (word2vec-google-news-300)
-wvs = KeyedVectors.load("pretrained-word2vec-keyedvectors.kv")
-# st = StandardScaler().fit_transform(wvs.vectors)
-# wvs.vectors = st
-norms = np.linalg.norm(wvs.vectors, axis=1)
+"""
+Uses sklearn to find the principal component axis
 
-if max(norms) - min(norms) > 0.0001:
-    print("Normalising")
-    wvs.vectors /= np.linalg.norm(wvs.vectors, axis=1)[:, np.newaxis]
-
-pairs = [('she', 'he'),
-('her', 'his'),
-('woman', 'man'),
-('Mary', 'John'),
-('herself', 'himself'),
-('daughter', 'son'),
-('mother', 'father'),
-('gal', 'guy'),
-('girl', 'boy'),
-('female', 'male')]
-
-def doPCA(pairs, embedding, num_components = 10):
-    matrix = []
-    for a, b in pairs:
-        center = (wvs[a] + wvs[b])/2
-        matrix.append(wvs[a] - center)
-        matrix.append(wvs[b] - center)
-    matrix = np.array(matrix)
-    pca = PCA(n_components = num_components)
+matrix - The matrix of vectors to do PCA on
+name - File name for outputting the bar chart (if display_chart=True)
+n_components - Number of components to find (and plot) in PCA
+display_chart - Display (and save) the bar chart of the top n_components explained variance ratios
+"""
+def principal_component_analysis(matrix, name="unknown", n_components=30, display_chart=False):
+    pca = PCA(n_components=n_components)
     pca.fit(matrix)
-    plt.bar(range(num_components), pca.explained_variance_ratio_)
-    plt.show()
-    return pca
 
-doPCA(pairs, wvs)
+    if display_chart:
+        plt.bar(range(n_components), pca.explained_variance_ratio_[:n_components])
+        #CHANGE BEFORE SUBMISSION TO DEF DIRECTORY
+        filename = f"./pca_charts/{datetime.now().isoformat().replace(':', '-')}_pca_{name}.png"
+        plt.savefig(filename)
+        plt.show()
 
-# Load the positive and negative words from the Sentiment Lexicon
-positive_words = create_lexicon_list(wvs, [positive_lexicon_path])
-negative_words = create_lexicon_list(wvs, [negative_lexicon_path])
-labelled_words = {**{ word: True for word in positive_words }, **{ word: False for word in negative_words }}
+    return pca.components_[0]
 
-# Now construct the matrix consisting of all of the positive and negative vectors
-positive_matrix = construct_w2v_matrix(wvs, positive_words)
-negative_matrix = construct_w2v_matrix(wvs, negative_words)
+"""
+Tests the classification based on the scalar projection on to the directional sentiment vector
 
-# Now get the most significant PCA component of the positive word vector matrix
-ncomps = 30
-pca_positive = PCA(n_components=ncomps)
-pca_positive.fit_transform(positive_matrix)
-ms_pca_comp_pos = pca_positive.components_[0]
-
-plt.bar(range(ncomps), pca_positive.explained_variance_ratio_[:ncomps])
-plt.show()
-
-# Do the same for the negative word vector matrix
-pca_negative = PCA(n_components=ncomps)
-pca_negative.fit_transform(negative_matrix)
-ms_pca_comp_neg = pca_negative.components_[0]
-
-plt.bar(range(ncomps), pca_negative.explained_variance_ratio_[:ncomps])
-plt.show()
-
-print(ms_pca_comp_neg.shape, ms_pca_comp_pos.shape)
-
-# Now take the signed difference btween these positive and negative components
-dsv =  ms_pca_comp_neg - ms_pca_comp_pos
-# dsv = dsv / np.sqrt(np.dot(dsv, dsv))
-
-# W = ["American", "Mexican", "German", "Italian", "French", "Polish", "admire", "awful", "disgusting", "lovely", "British", "English", "Scottish", "Irish", "Welsh"]
-#
-# for w in W:
-#     t = w
-#     if w not in wvs:
-#         t = w.lower()
-#         if w not in wvs:
-#             t = w.upper()
-#             if w not in wvs:
-#                 t = w.title()
-#                 if w not in wvs:
-#                     print(w, "Missing")
-#     print(t, np.dot(dsv, wvs.get_vector(t)))
-
-# Try project on some names
-
-def test_dsv_classifier(labelled_words, keyed_vectors, k):
-    all = 0
-    correct = 0
-    tp, fp = 0, 0
+positive_lexicon - Positive sentiment words
+negative_lexicon - Negative sentiment words
+word_vectors - The word2vec pre-trained model
+dsv - Directional Sentiment Vector
+"""
+def test_dsv_classification(positive_lexicon, negative_lexicon, word_vectors, dsv):
+    # I want to track the total predictions, correct predictions, true positives and
+    # false positives (for accuracy and precision)
+    total, correct, tp, fp = 0, 0, 0, 0
+    # Convert the two arrays to a single dictionary labelling each words with whether
+    # they are positive or negative in sentiment (True = positive, False = negative)
+    labelled_words = {**{ word: True for word in positive_lexicon }, **{ word: False for word in negative_lexicon }}
 
     for word in labelled_words:
-        if word.lower() not in keyed_vectors:
+        # Only interested in the lowercase versions for this
+        if word.lower() not in word_vectors:
             continue
 
-        is_positive = labelled_words[word]
-        projection = np.dot(k, keyed_vectors.get_vector(word.lower()))
-        y_star_hat = projection >= 0
+        # y_star is the real label
+        y_star = labelled_words[word]
+        # Scalar projection of the word vector on to the directional sentiment vector
+        scalar_projection = np.dot(dsv, word_vectors.get_vector(word.lower()))
+        # The sign determines the predicted class (True = positive, False = negative)
+        y_star_hat = scalar_projection >= 0
 
-        if y_star_hat == is_positive:
+        total += 1
+
+        # Correct
+        if y_star_hat == y_star:
+            correct += 1
+            # True Positive
             if y_star_hat == True:
                 tp += 1
+        else:
+            # False positive
+            if y_star_hat == True:
+                fp += 1
 
-            correct += 1
-        elif y_star_hat == True:
-            fp += 1
+    accuracy = correct / total * 100
+    precision = tp / (tp + fp) * 100
 
-        all += 1
+    return { "accuracy": accuracy, "precision": precision }
 
-    return (correct, all, tp / (fp + tp), (correct  / all * 100))
+def main():
+    # Load the pre-trained word2vec vectors
+    word_vectors = KeyedVectors.load(FILE_PATHS["pretrained-w2v-google-news"])
+    # Normalise the vectors
+    # CITATION NEEDED this is from a GitHub
+    word_vectors.vectors /= np.linalg.norm(word_vectors.vectors, axis=1)[:, np.newaxis]
 
-print(test_dsv_classifier(labelled_words, wvs, dsv))
+    # Load the positive and negative lexicons
+    positive_lexicon = load_lexicon(word_vectors, FILE_PATHS["positive_words"])
+    negative_lexicon = load_lexicon(word_vectors, FILE_PATHS["negative_words"])
+
+    # Convert them matrix form
+    positive_matrix = construct_w2v_matrix(word_vectors, positive_lexicon)
+    negative_matrix = construct_w2v_matrix(word_vectors, negative_lexicon)
+
+    # Find the most significant principal component axis for both matrices
+    # May also show the PCA chart depending on the parameters
+    positive_sig_comp = principal_component_analysis(positive_matrix, name="positive", display_chart=False)
+    negative_sig_comp = principal_component_analysis(negative_matrix, name="negative", display_chart=False)
+
+    # The directional sentiment vector is defined to be the signed difference between
+    # the principal component axis of the positive and negative sentiment matrices
+    dsv = negative_sig_comp - positive_sig_comp
+    # Normalise the vector as it will be used for vector and scalar projections
+    dsv = dsv / np.sqrt(np.dot(dsv, dsv))
+
+    test_results = test_dsv_classification(positive_lexicon, negative_lexicon, word_vectors, dsv)
+    print(test_results)
+
+main()
