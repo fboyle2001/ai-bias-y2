56f4a06519f24eda22a6bfc1e0b2c0391d1365e9
fboyle2001 <finlayboyle2001@gmail.com>
2021-04-02 17:25:47 +0100

    Slightly better but still not working correctly

--- sentiment_polarity.py
+++ sentiment_polarity.py
@@ -1,4 +1,5 @@
 import gensim.downloader as api
+import gensim.models
 from gensim.models.word2vec import Word2Vec, KeyedVectors
 import time
 from sklearn.decomposition import PCA
@@ -9,69 +10,91 @@ from matplotlib import pyplot as plt
 positive_lexicon_path = "./opinion-lexicon-English/positive-words.txt"
 negative_lexicon_path = "./opinion-lexicon-English/negative-words.txt"
 
-def create_lexicon_list(filepath):
+def create_lexicon_list(filepaths):
     cleaned_words = []
 
-    with open(filepath, "r") as file:
-        for line in file.readlines():
-            # The comments in the file start with a ;
-            if line.startswith(";"):
-                continue
+    for filepath in filepaths:
+        with open(filepath, "r") as file:
+            for line in file.readlines():
+                # The comments in the file start with a ;
+                if line.startswith(";"):
+                    continue
 
-            # They end with \n so strip any whitespace
-            cleaned_word = line.strip()
-            cleaned_words.append(cleaned_word)
+                # They end with \n so strip any whitespace
+                cleaned_word = line.strip()
+
+                if len(cleaned_word) == 0:
+                    continue
+
+                cleaned_words.append(cleaned_word)
 
     return cleaned_words
 
-def prepare_vector_matrix(keyed_vectors, word_list):
-    matrix = []
-    valid_words = []
-    parts = [[] for x in range(300)]
+def construct_w2v_matrix(wvs, words):
+    rows = []
 
-    for word in word_list:
-        vector = None
-
-        # Not all of the words will be in the corpus so skip them
-        try:
-            vector = keyed_vectors.get_vector(word)
-        except KeyError:
+    for word in words:
+        if word not in wvs:
             continue
 
-        if vector is None:
-            continue
+        rows.append(wvs.get_vector(word))
 
-        for i, part in enumerate(vector):
-            parts[i].append(vector[i])
+    return np.array(rows)
 
-        matrix.append(vector)
-        valid_words.append(word)
+# Load the vectors from word2vec trained on Google News (word2vec-google-news-300)
+wvs = KeyedVectors.load("pretrained-word2vec-keyedvectors.kv")
+st = StandardScaler().fit_transform(wvs.vectors)
+wvs.vectors = st
 
-    print(len(valid_words) / len(word_list))
-    # array will make them the rows so we need to transpose to get the vectors
-    # as the columns
-    return valid_words, np.transpose(np.array(matrix, dtype=np.float64))
+# Load the positive and negative words from the Sentiment Lexicon
+positive_words = create_lexicon_list([positive_lexicon_path])
+negative_words = create_lexicon_list([negative_lexicon_path])
+labelled_words = {**{ word: True for word in positive_words }, **{ word: False for word in negative_words }}
 
-def prepare_lexicon(keyed_vectors, filepath):
-    words = create_lexicon_list(filepath)
-    valid_words, matrix = prepare_vector_matrix(keyed_vectors, words)
+# Now construct the matrix consisting of all of the positive and negative vectors
+positive_matrix = construct_w2v_matrix(wvs, positive_words)
+negative_matrix = construct_w2v_matrix(wvs, negative_words)
 
-    return valid_words, matrix
+# Now get the most significant PCA component of the positive word vector matrix
+pca_positive = PCA()
+pca_positive.fit_transform(positive_matrix)
+ms_pca_comp_pos = pca_positive.components_[0]
 
-def prepare_keyed_vectors():
-    keyed_vectors = api.load("word2vec-google-news-300")
-    keyed_vectors.save("pretrained-word2vec-keyedvectors.kv")
-    keyed_vectors = api.load("glove-wiki-gigaword-300")
-    keyed_vectors.save("pretrained-glove-keyedvectors.kv")
+plt.bar(range(30), pca_positive.explained_variance_[:30])
+plt.show()
+
+# Do the same for the negative word vector matrix
+pca_negative = PCA()
+pca_negative.fit_transform(negative_matrix)
+ms_pca_comp_neg = pca_negative.components_[0]
+
+plt.bar(range(30), pca_negative.explained_variance_[:30])
+plt.show()
+
+print(ms_pca_comp_neg.shape, ms_pca_comp_pos.shape)
+
+# Now take the signed difference btween these positive and negative components
+dsv = ms_pca_comp_pos - ms_pca_comp_neg
+# dsv = dsv / np.sqrt(np.dot(dsv, dsv))
+
+W = ["American", "Mexican", "German", "Italian", "French", "Polish", "admire", "awful", "disgusting", "lovely", "British", "English", "Scottish", "Irish", "Welsh"]
+
+for w in W:
+    print(w, np.dot(dsv, wvs.get_vector(w.lower())))
+
+# Try project on some names
 
 def test_dsv_classifier(labelled_words, keyed_vectors, k):
     all = 0
     correct = 0
 
     for word in labelled_words:
+        if word not in keyed_vectors:
+            continue
+
         is_positive = labelled_words[word]
         projection = np.dot(k, keyed_vectors[word])
-        y_star_hat = projection > 0
+        y_star_hat = projection + 1 > 0
 
         if y_star_hat == is_positive:
             correct += 1
@@ -80,21 +103,4 @@ def test_dsv_classifier(labelled_words, keyed_vectors, k):
 
     return (correct, all, correct / all * 100)
 
-
-word_vectors = KeyedVectors.load("pretrained-word2vec-keyedvectors.kv")
-positive_words, positive_matrix = prepare_lexicon(word_vectors, positive_lexicon_path)
-negative_words, negative_matrix = prepare_lexicon(word_vectors, negative_lexicon_path)
-
-print(positive_matrix.shape, negative_matrix.shape)
-
-pca = PCA()
-pca.fit(positive_matrix)
-print(pca.singular_values_)
-plt.bar(range(30), pca.mean_[:30])
-plt.show()
-
-# labelled_words = {**{ word: True for word in positive_words }, **{ word: False for word in negative_words }}
-# res = test_dsv_classifier(labelled_words, word_vectors, dsv)
-# print(res)
-
-# plot_pca_bar_chart(positive_matrix)
+print(test_dsv_classifier(labelled_words, wvs, dsv))
