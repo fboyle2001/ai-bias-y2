2a1bd80cfaf1ec60b728d6c42aa2ddf06f71a6a4
fboyle2001 <finlayboyle2001@gmail.com>
2021-04-02 13:14:20 +0100

    The directional sentiment vector doesn't match up

--- sentiment_polarity.py
+++ sentiment_polarity.py
@@ -26,6 +26,8 @@ def create_lexicon_list(filepath):
 
 def prepare_vector_matrix(keyed_vectors, word_list):
     matrix = []
+    valid_words = []
+    parts = [[] for x in range(300)]
 
     for word in word_list:
         vector = None
@@ -39,91 +41,60 @@ def prepare_vector_matrix(keyed_vectors, word_list):
         if vector is None:
             continue
 
-        matrix.append(vector)
+        for i, part in enumerate(vector):
+            parts[i].append(vector[i])
 
+        matrix.append(vector)
+        valid_words.append(word)
+
+    print(len(valid_words) / len(word_list))
     # array will make them the rows so we need to transpose to get the vectors
     # as the columns
-    return np.transpose(np.array(matrix))
+    return valid_words, np.transpose(np.array(matrix, dtype=np.float64))
 
 def prepare_lexicon(keyed_vectors, filepath):
     words = create_lexicon_list(filepath)
-    matrix = prepare_vector_matrix(keyed_vectors, words)
+    valid_words, matrix = prepare_vector_matrix(keyed_vectors, words)
 
-    return words, matrix
+    return valid_words, matrix
 
-def get_principal_variances(matrix, n=30):
-    pca = PCA(n_components=n)
-    pca.fit(matrix)
-    return pca.explained_variance_
+def prepare_keyed_vectors():
+    keyed_vectors = api.load("word2vec-google-news-300")
+    keyed_vectors.save("pretrained-word2vec-keyedvectors.kv")
+    keyed_vectors = api.load("glove-wiki-gigaword-300")
+    keyed_vectors.save("pretrained-glove-keyedvectors.kv")
 
-def get_principal_components(matrix, n=1):
-    pca = PCA(n_components=n)
-    pca.fit(matrix)
-    return pca.components_
+def test_dsv_classifier(labelled_words, keyed_vectors, k):
+    all = 0
+    correct = 0
 
-def plot_pca_bar_chart(matrix, n=30, file=None, show=True):
-    principal_exp_variances = get_principal_variances(matrix, n)
-    plt.bar(np.arange(0, n), principal_exp_variances * 10)
+    for word in labelled_words:
+        is_positive = labelled_words[word]
+        projection = np.dot(k, keyed_vectors[word])
+        y_star_hat = projection > 0
 
-    if file is not None:
-        plt.savefig(file)
+        if y_star_hat == is_positive:
+            correct += 1
 
-    if show:
-        plt.show()
+        all += 1
 
-def project_vector():
-    pass
+    return (correct, all, correct / all * 100)
 
-# keyed_vectors = api.load("word2vec-google-news-300")
-# keyed_vectors.save("pretrained-word2vec-keyedvectors.kv")
-# keyed_vectors = api.load("glove-wiki-gigaword-300")
-# keyed_vectors.save("pretrained-glove-keyedvectors.kv")
 
 word_vectors = KeyedVectors.load("pretrained-word2vec-keyedvectors.kv")
 positive_words, positive_matrix = prepare_lexicon(word_vectors, positive_lexicon_path)
 negative_words, negative_matrix = prepare_lexicon(word_vectors, negative_lexicon_path)
 
-X = word_vectors.vectors
-pca = PCA(n_components=2)
-result = pca.fit_transform(X)
-print(pca.components_.shape)
-plt.bar(np.arange(0, 2), pca.explained_variance_)
+print(positive_matrix.shape, negative_matrix.shape)
+
+pca = PCA()
+pca.fit(positive_matrix)
+print(pca.singular_values_)
+plt.bar(range(30), pca.mean_[:30])
 plt.show()
 
-# result = pca.fit_transform()
+# labelled_words = {**{ word: True for word in positive_words }, **{ word: False for word in negative_words }}
+# res = test_dsv_classifier(labelled_words, word_vectors, dsv)
+# print(res)
 
-# pos_pca = PCA()
-# pos_pca.fit(positive_matrix)
-# first_pos_pc = pos_pca.components_[:,0]
-#
-# neg_pca = PCA()
-# neg_pca.fit(negative_matrix)
-# first_neg_pc = neg_pca.components_[:,0]
-#
-# directional_sentiment_vector = first_pos_pc - first_neg_pc
-# normalised_dsv = directional_sentiment_vector / np.linalg.norm(directional_sentiment_vector)
-#
-# polish_projection_scalar = np.dot(word_vectors["Polish"], normalised_dsv)
-# american_projection_scalar = np.dot(word_vectors["American"], normalised_dsv)
-# french_projection_scalar = np.dot(word_vectors["French"], normalised_dsv)
-#
-# print(polish_projection_scalar)
-# print(american_projection_scalar)
-# print(french_projection_scalar)
-
-# plot_pca_bar_chart(positive_matrix, file="fig.png")
-
-# pos_pcs = get_principal_components(positive_matrix)
-# neg_pcs = get_principal_components(negative_matrix)
 # plot_pca_bar_chart(positive_matrix)
-# #
-# # print(pos_pcs)
-# # print(neg_pcs)
-# print(pos_pcs.shape)
-# print(neg_pcs.shape)
-
-# directional_sentiment_vector = pos_pcs - neg_pcs
-# print(directional_sentiment_vector.shape)
-# print(directional_sentiment_vector)
-# plt.bar(np.arange(0, 30), directional_sentiment_vector)
-# plt.show()
